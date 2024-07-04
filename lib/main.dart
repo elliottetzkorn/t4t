d6b3f390f45342b3edc7b09fb507a533ca42cc7b
Elliott Etzkorn <mail@elliottetzkorn.com>
2024-07-04 11:09:27 -0400

    More env updates for cross platform support

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,9 @@ Future<void> safelyInitializeFirebase() async {
     await Firebase.initializeApp(
       options: FirebaseOptions(
         apiKey: dotenv.env['FIREBASE_API_KEY']!,
-        appId: dotenv.env['FIREBASE_APP_ID']!,
+        appId: Platform.isIOS
+            ? dotenv.env['FIREBASE_IOS_APP_ID']!
+            : dotenv.env['FIREBASE_ANDROID_APP_ID']!,
         messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
         projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
         storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
