ad5ab624d1995210fe0204377165b029541b4454
Elliott Etzkorn <mail@elliottetzkorn.com>
2024-07-23 13:31:32 -0400

    Add back in crashylitics script and designate tracking (#107)

--- lib/main.dart
+++ lib/main.dart
@@ -66,19 +66,18 @@ Future<void> setPrefs() async {
 Future<void> safelyInitializeFirebase() async {
   try {
     await Firebase.initializeApp(
-      options: FirebaseOptions(
-        apiKey: dotenv.env['FIREBASE_API_KEY']!,
-        appId: Platform.isIOS
-            ? dotenv.env['FIREBASE_IOS_APP_ID']!
-            : dotenv.env['FIREBASE_ANDROID_APP_ID']!,
-        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-        projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
-        androidClientId: dotenv.env['FIREBASE_ANDROID_CLIENT_ID']!,
-        iosClientId: dotenv.env['FIREBASE_IOS_CLIENT_ID']!,
-        iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID']!,
-      ),
-    );
+        options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      appId: Platform.isIOS
+          ? dotenv.env['FIREBASE_IOS_APP_ID']!
+          : dotenv.env['FIREBASE_ANDROID_APP_ID']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+      androidClientId: dotenv.env['FIREBASE_ANDROID_CLIENT_ID']!,
+      iosClientId: dotenv.env['FIREBASE_IOS_CLIENT_ID']!,
+      iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID']!,
+    ));
 
     await FirebaseMessaging.instance.requestPermission(
       alert: true,
