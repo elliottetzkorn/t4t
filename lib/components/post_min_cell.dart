d1ed36367fc9962a0447d983314d1ff4265061f9
Lydia Scarf <lydia@oldgrowth.city>
2024-08-01 11:30:04 +0200

    Revert changes from old approach

--- lib/components/post_min_cell.dart
+++ lib/components/post_min_cell.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:t4t/data/post_min_data.dart';
 import 'package:t4t/design_system/system_linkable_text.dart';
 import 'package:t4t/design_system/system_text.dart';
 import 'package:t4t/enums/text_size_enum.dart';
 import 'package:t4t/constants.dart';
 
-class PostMinCell extends ConsumerWidget {
+class PostMinCell extends StatelessWidget {
   const PostMinCell({super.key, required this.post});
 
   final PostMinData post;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SystemText(
-              underline: true, text: post.title, size: TextSizeEnum.eighteen),
+            underline: true,
+            text: post.title,
+            size: TextSizeEnum.eighteen,
+          ),
           const SizedBox(
             height: spacingThree,
           ),
