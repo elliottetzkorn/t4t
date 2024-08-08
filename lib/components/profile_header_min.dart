d081cc4f1d303f21e6a89be432ad3f7d469f21ef
Elliott Etzkorn <mail@elliottetzkorn.com>
2024-08-08 13:39:04 -0400

    Update supporter cta on user sheets

--- lib/components/profile_header_min.dart
+++ lib/components/profile_header_min.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:t4t/components/supporter_sheet.dart';
+import 'package:t4t/components/system_tap.dart';
 import 'package:t4t/constants.dart';
 import 'package:t4t/data/profile_min_data.dart';
 import 'package:t4t/design_system/system_text.dart';
 import 'package:t4t/extensions/color_extensions.dart';
 import 'package:t4t/extensions/strings_extensions.dart';
+import 'package:t4t/providers/profile_provider.dart';
 import 'package:t4t/providers/resolved_brightness_provider.dart';
 
 class ProfileHeaderMin extends ConsumerWidget {
@@ -34,13 +37,29 @@ class ProfileHeaderMin extends ConsumerWidget {
                 text: '🌱${AppLocalizations.of(context)!.staff}',
               )),
         if (profile.supporter && !profile.id.isEllId())
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: spacingTwo),
-              child: SystemText(
-                color: Theme.of(context).colorScheme.error,
-                maxLines: 1,
-                text: '✨${AppLocalizations.of(context)!.supporter}',
-              ))
+          SystemTap(
+              onTap: ref.read(profileProvider).value != null &&
+                      ref.read(profileProvider).value!.supporter
+                  ? null
+                  : () => SupporterSheet().show(context, ref),
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: spacingTwo),
+                  child: Row(
+                    children: [
+                      const SystemText(
+                        text: '✨',
+                      ),
+                      SystemText(
+                        color: Theme.of(context).colorScheme.error,
+                        maxLines: 1,
+                        underline: ref.read(profileProvider).value != null &&
+                                ref.read(profileProvider).value!.supporter
+                            ? false
+                            : true,
+                        text: AppLocalizations.of(context)!.supporter,
+                      )
+                    ],
+                  )))
       ],
     );
   }
